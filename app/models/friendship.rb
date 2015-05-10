cbf1ed57db779e03d2b3c9b9164e8e3545721268
goisesan <m220274@gmail.com>
2015-05-11 00:40:08 +0900

    helper 修正

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,9 @@
 class Friendship < ActiveRecord::Base
   belongs_to :from_user, :class_name => 'User'
   belongs_to :to_user, :class_name => 'User'
+
+  def self.follow?(user, current_user)
+    self.find_by(to_user_id: user.id, from_user_id: current_user.id).nil?
+  end
+
 end
